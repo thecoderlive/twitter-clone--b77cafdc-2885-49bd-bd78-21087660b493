b543e4fa43406daa5648648574327359d1e4cd43
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-04-14 12:24:03 +0530

    Initial commit for file: HomeScreen.js

---
+++ HomeScreen.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect, useReducer } from 'react'
+import { ActivityIndicator, ScrollView, StyleSheet, View } from 'react-native'
+
+import { actionCreators, initialState, reducer } from './reducer'
+import { api } from './api'
+import { data } from './data'
+import * as items from './home_screen_data'
+import Tweets from './Tweets'
+
+function HomeScreen({ navigation, route }){ 
+const url = (api.home_screen ?? "home_screen/") + (route?.params?.id ?? '')
+const [state, dispatch] = useReducer(reducer, initialState)
+
+const { item, history, loading, error } = state
+
+
+
+async function getItem() {
+      dispatch(actionCreators.loading())
+
+      try {
+        if (url in history){
+           dispatch(actionCreators.local(history[url]))
+        } else if (url.indexOf('http') > -1){
+          const response = await fetch(url)
+          const json = await response.json()
+          if(json){
+            dispatch(actionCreators.success(route.params?.id ? json : json, url))
+          }   
+        } else {
+          const json = route.params?.id ? data[route.params?.id] : items.item
+          dispatch(actionCreators.success(json, url))
+        }
+      } catch (e) {
+        dispatch(actionCreators.failure())
+      }
+    }
+
+useEffect(() => {
+    getItem();
+}, []);
+  
+if (loading) {
+    return (
+        <View style={styles.center}>
+        <ActivityIndicator animating={true} />
+        </View>
+    )
+}
+
+return(
+<ScrollView style={styles.home_screen} showsVerticalScrollIndicator={false}>
+<Tweets item={'tweets' in item ? item.tweets: item} navigation={navigation}/>
+</ScrollView>
+)}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    "center": {
+        "flex": 1,
+        "justifyContent": "center",
+        "alignItems": "center"
+    }
+});
