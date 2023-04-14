4790fd9d9fceba00a3b031068af01d81daf7dc70
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-04-14 12:24:03 +0530

    Initial commit for file: RootNavigation.js

---
+++ RootNavigation.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Button, Text, View, StyleSheet } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+import HomeScreen from './HomeScreen'
+import ExploreScreen from './ExploreScreen'
+
+const Root = createStackNavigator()
+
+export default function App() {
+return (
+<NavigationContainer>
+<Root.Navigator
+    initialRouteName="HomeScreen"
+    screenOptions={{
+        headerMode:'screen',
+        headerStyle: { backgroundColor: 'whitesmoke', height: 40 },
+        title: null,
+        cardOverlayEnabled:true,
+        cartStyle:{flex:1},
+        animationEnabled:true,
+        gestureEnabled:true
+    }}
+    >
+<Root.Screen name="HomeScreen" component={HomeScreen} />
+<Root.Screen name="ExploreScreen" component={ExploreScreen} />
+</Root.Navigator>
+<Text style={{height: 1}}>.</Text>
+</NavigationContainer>
+)}
+
+const styles = StyleSheet.create({
+  screen: {
+    marginTop: 40,
+    alignItems: 'center',
+  },
+})
